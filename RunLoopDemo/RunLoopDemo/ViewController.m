642a58b68b92118fb55939fa88e794f2169f716d
prg_change <prg_change@163.com>
2018-07-20 14:58:21 +0800

    完善代码

--- RunLoopDemo/RunLoopDemo/ViewController.m
+++ RunLoopDemo/RunLoopDemo/ViewController.m
@@ -10,7 +10,8 @@
 #import "RGThread.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) RGThread *thread;
+@property (nonatomic, assign,getter=isStoped) BOOL stoped;
 @end
 
 @implementation ViewController
@@ -18,16 +19,66 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    RGThread *thread = [[RGThread alloc] initWithTarget:self selector:@selector(run) object:nil];
-    [thread start];
+    __weak typeof(self) weakSelf = self;
+    
+    self.stoped = NO;
+    
+    self.thread = [[RGThread alloc] initWithBlock:^{
+        NSLog(@"----begin----%@",[NSThread currentThread]);
+        //往RunLoop中添加source/Timer/Observer
+        [[NSRunLoop currentRunLoop] addPort:[[NSPort alloc] init] forMode:NSDefaultRunLoopMode];
+        while (weakSelf && !weakSelf.isStoped) {
+            [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+        }
+        NSLog(@"----end----%@",[NSThread currentThread]);
+        
+    }];
+    [self.thread start];
     
 }
 
--(void)run {
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    if (self.thread == nil) {
+        return;
+    }
+    // wait 如果是YES,意思就是执行完test，再回来执行后面的代码，如果是NO，就是不等执行完test，就直接执行
+    [self performSelector:@selector(test) onThread:self.thread withObject:nil waitUntilDone:NO];
+    NSLog(@"1111");
+}
+//停止
+- (IBAction)stop {
+    
+    if (self.thread == nil) {
+        return;
+    }
+    // wait 如果是YES,意思就是执行完test，再回来执行后面的代码，如果是NO，就是不等执行完test，就直接执行
+    [self performSelector:@selector(stopThread) onThread:self.thread withObject:nil waitUntilDone:YES];
+}
+
+//停止RunLoop
+-(void)stopThread {
+    
+    self.stoped = YES;
+    //停止RunLoop
+    CFRunLoopStop(CFRunLoopGetCurrent());
+    NSLog(@"%s  %@",__func__,[NSThread currentThread]);
+    
+    //清空线程
+    self.thread = nil;
+    
+}
+
+//执行的任务
+-(void)test {
     
     NSLog(@"%s---%@",__func__,[NSThread currentThread]);
-    
 }
 
+-(void)dealloc
+{
+    NSLog(@"%s",__func__);
+    [self stop];
+}
 
 @end
